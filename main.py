f11f5205f50ff03e67a5783c2e147acfee8d6125
kk06112001 <kk1455217@gmail.com>
2025-03-30 17:49:59 +0530

    data transformation done

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage1_dataingestion import DataIngestionTrainingPipeLine
+from src.textSummarizer.pipeline.stage2_datatransformation import DataTransformationTrainingPipeLine
 
 stage_name="Data Ingestion"
 try:
     logger.info(f"{'>>'*20} {stage_name} started {'<<'*20}")
     data_ingestion=DataIngestionTrainingPipeLine()
     data_ingestion.initiate_data_ingestion()
+    logger.info(f"{'>>'*20} {stage_name} completed {'<<'*20}")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+stage_name="Data Transformation"
+try:
+    logger.info(f"{'>>'*20} {stage_name} started {'<<'*20}")
+    data_transformation=DataTransformationTrainingPipeLine()
+    data_transformation.initiate_data_transformation()
     logger.info(f"{'>>'*20} {stage_name} completed {'<<'*20}")
 
 except Exception as e:
