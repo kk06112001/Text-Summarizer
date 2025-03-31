32dddb044f73a018fd187099e9cc6196b4da0fb4
kk06112001 <kk1455217@gmail.com>
2025-03-31 17:21:38 +0530

    final model

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#   TextSummarizer USing Huggingface
+
 
 ### Workflows 
 
@@ -9,3 +9,4 @@
 5. Update the components- Data Ingestion,Data Transformation, Model Trainer
 6. Create our Pipeline-- Training Pipeline,PRediction Pipeline
 7. Front end-- Api's, Training APi's, Batch Prtediction API's
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage1_dataingestion import DataIngestionTrainingPipeLine
 from src.textSummarizer.pipeline.stage2_datatransformation import DataTransformationTrainingPipeLine
-
+from src.textSummarizer.pipeline.stage3_modeltrainer import ModelTrainerTrainingPipeLine
+from src.textSummarizer.pipeline.stage4_modelevaluation import ModelEvaluationTrainingPipeLine
 stage_name="Data Ingestion"
 try:
     logger.info(f"{'>>'*20} {stage_name} started {'<<'*20}")
@@ -18,6 +19,28 @@ try:
     logger.info(f"{'>>'*20} {stage_name} started {'<<'*20}")
     data_transformation=DataTransformationTrainingPipeLine()
     data_transformation.initiate_data_transformation()
+    logger.info(f"{'>>'*20} {stage_name} completed {'<<'*20}")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+stage_name="Model Training"
+try:
+    logger.info(f"{'>>'*20} {stage_name} started {'<<'*20}")
+    model_trainer=ModelTrainerTrainingPipeLine()
+    model_trainer.initiate_model_trainer()
+    logger.info(f"{'>>'*20} {stage_name} completed {'<<'*20}")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+stage_name="Model Evaluation"
+try:
+    logger.info(f"{'>>'*20} {stage_name} started {'<<'*20}")
+    model_eval=ModelEvaluationTrainingPipeLine()
+    model_eval.initiate_model_evaluation()
     logger.info(f"{'>>'*20} {stage_name} completed {'<<'*20}")
 
 except Exception as e:
